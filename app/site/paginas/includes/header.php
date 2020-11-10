988f0785809b2c14c3a970d7e47b2a5dfe1098bb
ReginaldojLima <reginaldojlima963@gmail.com>
2020-11-10 14:50:55 -0300

    Página Header

---
+++ app/site/paginas/includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto final</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="app/assets/bootstrap/css/bootstrap.min.css">
+
+    <!-- Icomoon -->
+    <link rel="stylesheet" href="app/assets/icomoon/style.css">
+
+    <!-- outros -->
+    <link rel="stylesheet" href="">
+
+    <!-- Meu CSS -->
+    <link rel="stylesheet" href="app/assets/css/meuCSS.css">
+
+
+</head>
+
+<body>
+
+    <!-- Header -->
+<h1>Header</h1>
--- index.php
+++ index.php
@@ -1,29 +1,10 @@
-<!DOCTYPE html>
-<html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto final</title>
+<?php 
+include_once "app/site/paginas/includes/header.php";
+?>
+    <!-- Navegação -->
+    <!-- Fim Navegação -->
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="app/assets/bootstrap/css/bootstrap.min.css">
-
-    <!-- Icomoon -->
-    <link rel="stylesheet" href="app/assets/icomoon/style.css">
-
-    <!-- outros -->
-    <link rel="stylesheet" href="">
-
-    <!-- Meu CSS -->
-    <link rel="stylesheet" href="">
-
-
-</head>
-
-<body>
-
-    <!-- Header -->
     <!-- Fim Header -->
 
 
